8b8a8e920544c5916e5e552dd506d6ca18702581
Tony Guan <tonyg06@msn.com>
2020-02-27 18:28:16 +1100

    Updates

---
+++ precache-manifest.fe2ed41f13a6955a2e96f6ed2cfd41b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe53625db87d21fb0f1790dcff9af361",
+    "url": "/TG0606/money-manager.git/index.html"
+  },
+  {
+    "revision": "90e8bc8bd6d050e15c2b",
+    "url": "/TG0606/money-manager.git/static/css/2.008c07f9.chunk.css"
+  },
+  {
+    "revision": "1d133d4a4811014a70fb",
+    "url": "/TG0606/money-manager.git/static/css/main.c79c0ba4.chunk.css"
+  },
+  {
+    "revision": "90e8bc8bd6d050e15c2b",
+    "url": "/TG0606/money-manager.git/static/js/2.ea47c4d8.chunk.js"
+  },
+  {
+    "revision": "f12839b2eb96790ff6590debcc216caa",
+    "url": "/TG0606/money-manager.git/static/js/2.ea47c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d133d4a4811014a70fb",
+    "url": "/TG0606/money-manager.git/static/js/main.a56210d0.chunk.js"
+  },
+  {
+    "revision": "9ceda630e02f6d771786",
+    "url": "/TG0606/money-manager.git/static/js/runtime-main.ab38b8d6.js"
+  },
+  {
+    "revision": "89ef5fc24195a1711a9408541d9bd149",
+    "url": "/TG0606/money-manager.git/static/media/imageOne.89ef5fc2.png"
+  },
+  {
+    "revision": "3f5af817cbb0917c60eee85809d0e1de",
+    "url": "/TG0606/money-manager.git/static/media/imageThree.3f5af817.jpg"
+  },
+  {
+    "revision": "4f6847a558e5b3f1ff22cdc0e7ea55ce",
+    "url": "/TG0606/money-manager.git/static/media/imageTwo.4f6847a5.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TG0606/money-manager.git/precache-manifest.fe2ed41f13a6955a2e96f6ed2cfd41b0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TG0606/money-manager.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
